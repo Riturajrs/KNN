3ff7074b7aede34c417a68f9fbcec1ec75df4b38
Rituraj Seal <rituraj2001.rs@gmail.com>
2021-09-23 21:49:33 +0530

    Model changed

--- main.py
+++ main.py
@@ -1,49 +1,29 @@
 import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
 
-
-class KNearestNeighbour:
-    def __init__(self, k):
-        self.k = k
-
-    def train(self, x, y):
-        self.x_train = x
-        self.y_train = y
-
-    def predict(self, x_test):
-        distance = self.compute_distance(x_test)
-        return self.predict_labels(distance)
-
-    def compute_distance(self, x_test):
-        num_test = x_test.shape[0]
-        num_train = self.x_train.shape[0]
-        distance = np.zeros((num_test, num_train))
-
-        for i in range(num_test):
-            for j in range(num_train):
-                distance[i, j] = np.sqrt(np.sum((x_test[i, :] - self.x_train[j, :])**2))
-        return distance
-
-    def predict_labels(self, distance):
-        num_test = distance.shape[0]
-        y_predict = np.zeros(num_test)
-        for i in range(num_test):
-            y_indices = np.argsort(distance[i, :])
-            k_closest = self.y_train[y_indices[:self.k]].astype(int)
-            y_predict[i] = np.argmax(np.bincount(k_closest))
-
-        return y_predict
-
-if __name__ == '__main__':
-    x = np.loadtxt("example_data/data.txt", delimiter=',')
-    y = np.loadtxt("example_data/targets.txt")
-    k1 = 0
-    matches = 0
-    for n in range(1, y.shape[0]):
-        KNN = KNearestNeighbour(k=n)
-        KNN.train(x, y)
-        y_predict = KNN.predict(x)
-        if sum(y == y_predict) > matches:
-            matches = sum(y == y_predict)
-            k1 = n
-    print(f"Ideal k value : {k1} \nAccuracy : {matches/y.shape[0]*100}%")
-
+df1 = pd.read_csv('glass.csv')
+X = df1[['RI','Na','Mg','Al','Si','K','Ca','Ba','Fe']]
+y = df1['Type']
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.65)
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+scores = {}
+scores_list = []
+k1 = 0
+acc = 0
+for k in range(1,25):
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train,y_train)
+    y_pred = knn.predict(X_test)
+    scores[k] = metrics.accuracy_score(y_test,y_pred)
+    scores_list.append(scores[k])
+    if scores[k] > acc:
+        acc = scores[k]
+        k1 = k
+print(k1," ", acc)
+plt.plot(range(1,25),scores_list)
+plt.xlabel("Value of K for KNN")
+plt.ylabel('Testing accuracy')
+plt.show()
